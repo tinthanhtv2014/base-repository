1415a9e9e0a7de53df03e4361765b14b4324d658
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2025-04-30 10:50:53 +0700

    update

---
+++ base-repository/lib/index.js
@@ -0,0 +1,8 @@
+// const BaseRepository = require('../src/responsivetory');
+// module.exports = BaseRepository;
+
+
+
+require("bytenode");
+const BaseRepository = require("./responsivetory.jsc");
+module.exports = BaseRepository
